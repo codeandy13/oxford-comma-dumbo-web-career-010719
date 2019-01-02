8a3836090be16821ad4a2cc394aae52373b9764c
Andrew Kim <akim618@gmail.com>
2019-01-02 17:39:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
+  if array.length == 2
+    array.join( " and ")
+  elsif array.length == 1
+    array.join
+  else
+    output = ""
 
+    counter = 0
+    loop do
+      counter = counter + 1
+      if counter = array.length
+      return array.join(", and ")
+      break
+    end
+  end
+
+
+  end
 end
